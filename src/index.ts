7b07d4936fcace98dc87571314b31dbbbbd65fe8
consti <5172+consti@users.noreply.github.com>
2021-12-21 12:42:56 +0700

    Fix hidden element autogrow Taken from stimulus-textarea-autogrow/issues/5

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,10 @@ export default class extends Controller {
 
     this.element.addEventListener('input', this.autogrow)
     window.addEventListener('resize', this.onResize)
+
+    if(this.element.scrollHeight == 0) { // element is initially hidden
+      this.element.style.height = `${(this.linesCount() * this.lineHeight()) + this.getPaddingsSum() + 20}px`
+    }
   }
 
   disconnect (): void {
@@ -35,5 +39,36 @@ export default class extends Controller {
   autogrow (): void {
     this.element.style.height = 'auto' // Force re-print before calculating the scrollHeight value.
     this.element.style.height = `${this.element.scrollHeight}px`
+  }
+
+  getStyle(el, styleProp) {
+    let y = null
+    if (el.currentStyle)
+      y = el.currentStyle[styleProp]
+    else if (window.getComputedStyle)
+      y = document.defaultView.getComputedStyle(el,null).getPropertyValue(styleProp)
+    return y
+  }
+
+  linesCount() {
+    return this.element.value.split("\n").length
+  }
+
+  lineHeight() {
+    let height_px = this.getStyle(this.element, 'line-height')
+    return parseInt(height_px) || 0
+  }
+
+  getPaddingsSum() {
+    let sum = 0
+    let padding_top_px = this.getStyle(this.element, 'padding-top')
+    if(padding_top_px) {
+      sum += parseInt(padding_top_px) || 0
+    }
+    let padding_bottom_px = this.getStyle(this.element, 'padding-bottom')
+    if(padding_bottom_px) {
+      sum += parseInt(padding_bottom_px) || 0
+    }
+    return sum
   }
 }
